ada88342c1778e12ed515f86bcba0e1e3e80c935
CARLOS EDUARDO DO NASCIMENTO DE PAIVA <carloseduardonascimento0459@gmail.com>
2023-08-14 19:34:13 -0300

    Maior Número

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1;
+int num2;
+
+Console.WriteLine("Insira o primeiro número:");
+num1= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira o segundo número:");
+num2= int.Parse(Console.ReadLine());
+
+if(num1> num2){
+    Console.WriteLine("O numero {0}, é maior que o número {1}.",num1,num2);
+}else{
+    Console.WriteLine("O número {0}, é menor que o número {1}.",num1,num2);
+}
